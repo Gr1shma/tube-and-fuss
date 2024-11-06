e8679a484c880f22d6b9c54ab82416fd94cd0127
Grishma Dhakal <dhakalgrishma5@gmail.com>
2024-11-06 10:55:32 +0545

    build: some more user controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,3 +1,5 @@
+import jwt from "jsonwebtoken";
+
 import { cookiesOptions } from "../constant.js";
 import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
@@ -107,8 +109,139 @@ const logoutUser = asyncHandler( async(req, res) => {
         json(new ApiResponse(200, {}, "User logged out sucessfully"));
 })
 
+
+const refreshAccessToken = asyncHandler(async(req, res) => {
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken;
+    if(!incomingRefreshToken){
+        throw new ApiError(401, "unauthorized request");
+    }
+    try {
+        const decodedToken = jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET);
+        const user = await User.findById(decodedToken?._id);
+        if(!user){
+            throw new ApiError(401, "Invalid Refresh Token");
+        }
+        if(incomingRefreshToken !== user?.refreshToken){
+            throw new ApiError(401, "Refresh token is expired or used");
+        }
+
+        const {accessToken , refreshToken } = await generateAccessAndRefreshToken(user._id);
+        return res.
+            status(200).
+            cookie("accessToken", accessToken, cookiesOptions).
+            cookie("refreshToken", refreshToken, cookiesOptions).
+            json(new ApiResponse(
+                200,
+                {accessToken, refreshToken},
+                "Access token refresed"
+            ))
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid refresh token"); 
+    }
+})
+
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+    const {oldPassword, newPassword} = req.body;
+
+    const user = await User.findById(req.user?._id);
+
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+    if(!isPasswordCorrect){
+        throw new ApiError(400, "Wrong Old Password");
+    }
+
+    user.password = newPassword;
+    await user.save({validateBeforeSave: false})
+    return res.
+        status(200).
+        json(new ApiResponse(200, {}, "Password changed Sucessfully"));
+})
+
+const getCurrentUser = asyncHandler(async(req, res) => {
+    return res.
+        status(200).
+        json(new ApiResponse(200, req.user, "Current user fetched sucessfully"));
+})
+
+const updateAccountDetails = asyncHandler(async(req, res) => {
+    const {fullName, email} = req.body;
+    if(!fullName || !email){
+        throw new ApiError(400, "All fields are required");
+    }
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                fullName,
+                email,
+            }
+        },{
+            new: true // get updated data i.e changed data
+        }
+    ).select("-password")
+    return res.
+        status(200).
+        json(new ApiResponse(200, user, "Account details Updated Sucessfully"));
+})
+
+const updateUserAvatar = asyncHandler(async(req, res) => {
+    const avatarLocalPath = req.file?.path;
+    if(!avatarLocalPath){
+        throw new ApiError(404, "Avatar file not found");
+    }
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+    if(!avatar){
+        new ApiError(400, "Error while uploading on avatar");
+    }
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                avatar: avatar.url,
+            }
+        }, {
+            new: true,
+        }
+    ).select("-password");
+    return res.
+        status(200).
+        json(new ApiResponse(200, user, "Avatar updated Sucessfully"));
+})
+
+const updateUserCoverImage = asyncHandler(async(req, res) => {
+    const coverImageLocalPath = req.file?.path;
+    if(!coverImageLocalPath){
+        throw new ApiError(404, "Cover image file not found");
+    }
+    const cover = await uploadOnCloudinary(coverImageLocalPath);
+    if(!cover){
+        new ApiError(400, "Error while uploading on avatar");
+    }
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                coverImage: cover.url,
+            }
+        }, {
+            new: true,
+        }
+    ).select("-password");
+    return res.
+        status(200).
+        json(new ApiResponse(200, user, "Avatar updated Sucessfully"));
+})
+
+// TODO: Delete the old avatar and cover image after the sucessfully updated;
+
 export {
     registerUser,
     loginUser,
     logoutUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 const router = Router();
@@ -11,5 +11,13 @@ router.route("/register").post(upload.fields([
 
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJwt,logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/current-user").get(verifyJwt, getCurrentUser);
+router.route("/change-password").post(verifyJwt, changeCurrentPassword);
+router.route("/update-account").patch(verifyJwt, updateAccountDetails);
+
+router.route("/change-avatar").patch(verifyJwt, upload.single("avatar"), updateUserAvatar);
+router.route("/change-cover-image").patch(verifyJwt, upload.single("coverImage"), updateUserCoverImage);
 
 export default router;
